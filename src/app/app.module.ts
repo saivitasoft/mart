9d2969e706554a69c46cf6f9e764592aeb2a8e85
saivitasoft <saikumarvita@gmail.com>
2023-11-08 17:12:39 +0530

    components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RComponent } from './r/r.component';
+import { MensComponent } from './mens/mens.component';
+import { WomensComponent } from './womens/womens.component';
+import { ElectronicsComponent } from './electronics/electronics.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RComponent,
+    MensComponent,
+    WomensComponent,
+    ElectronicsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/mens/mens.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mens',
+  templateUrl: './mens.component.html',
+  styleUrls: ['./mens.component.css']
+})
+export class MensComponent {
+
+}
